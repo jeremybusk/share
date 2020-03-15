cccd654da1448e5ee3444e6810e6fef05af96bd2
Jeremy Busk <jeremybusk@gmail.com>
2020-03-15 17:39:12 -0600

    Update sec-defm14a.py

--- sec-defm14a.py
+++ sec-defm14a.py
@@ -1,50 +1,56 @@
 #!/usr/bin/env python3
-import subprocess
-from bs4 import BeautifulSoup as bs
-# import requests
-import re
-import os
+from bs4 import BeautifulSoup, SoupStrainer
+import httplib2
 import sys
+import re
+import subprocess
+import os
 
 
 def main():
-    get_forms()
-
-
-def get_forms():
+    filename = 'urls.txt'
     downloads_dir = 'downloads'
     if not os.path.exists(downloads_dir):
         os.makedirs(downloads_dir)
+    http = httplib2.Http()
     pages = range(55)
+    url_list = []
+    open(filename, 'w').close()
     for page in pages:
-        print(page)
-        filename = f'{page}.html'
         index_url = f"https://sec.report/Document/Header/?formType=DEFM14A&page={page}"
-        curl(index_url, filename)
-        # with open(f'./{filename}', 'r') as f:
-        with open(filename, 'r') as f:
-            html_page = f.read()
-        soup = bs(html_page, 'html.parser')
 
-        count = 0
-        for a in soup.findAll('a'):
-            href = a.get('href')
-            # Document/Search/?formType=FormDEFM14A
-            # /Document/Header/?formType=DEFM14A
-            if 'Document' in href and 'DEFM14A' not in href and 'sec.report' not in href:
-                url = f"https://sec.report/{href}"
-                filename = url.split('/')[-2] + '.html'
-                filename = f"{downloads_dir}/{filename}"
-                count += 1
-                print(f"{count} url: {url} filename: {filename}")
-                curl(url, filename)
+        status, response = http.request(index_url)
 
+        p1 = re.compile('/Document/(\d{10}-\d{2}-\d{6}/)')
+        p2 = re.compile('(\d{10}-\d{2}-\d{6})')
+        for link in BeautifulSoup(response, features="html.parser", parse_only=SoupStrainer('a')):
+            if link.has_attr('href'):
+                href = link['href']
+                m = p1.search(link['href'])
+                if m:
+                    url_path = m[0]
+                    if len(url_path) != 0:
+                        url = f"https://sec.report/{url_path}"
+                        url_list.append(url)
+
+    with open('all-urls.txt', 'w') as f:
+        for url in url_list:
+            f.write(f"{url}\n")
+
+    url_list = list(set(url_list))  # unique the list
+
+    with open(filename, 'w') as f:
+        for url in url_list:
+            doc_uid = p2.search(url)[0]
+            html_file = f"{downloads_dir}/{doc_uid}.html"
+            curl(url, html_file)
+            f.write(f"{url}\n")
 
 
 def curl(url, filename):
-    # cmd = f"curl {url} > {filename}"
+    print(url)
+    print(filename)
     cmd = f"curl -o {filename} {url}"
-    # cmd = shlex.split(cmd)
     process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     print(stdout)
@@ -52,3 +58,40 @@ def curl(url, filename):
 
 if __name__ == "__main__":
     main()
+
+
+# cmd = shlex.split(cmd)
+# f.write(url)
+# # open(filename, 'w').close()
+# f = open(filename, 'a')
+# f.close()
+
+
+
+
+
+# TRASH
+# list(set(output))
+#     for url in url_list:
+#         if url not in url_list:
+#             url_list.append(url)
+#
+# s = '/Document/0000912057-00-002073/'
+# # p = re.compile('ab*')
+# # p = re.compile('/Document/[0-9]10*')
+# p = re.compile('/Document/(\d{10}-\d{2}-\d{6}/)')
+# # p = re.compile('ab*', re.IGNORECASE)
+# m = p.search(s)
+# print(m[0])
+# status, response = http.request('http://www.nytimes.com')
+
+
+    # filename = f'{page}.html'
+# sys.exit()
+
+# status, response = http.request('https://sec.report/Document/Header/?formType=DEFM14A&page=1')
+#     pages = range(55)
+#     for page in pages:
+#         print(page)
+#         filename = f'{page}.html'
+#         index_url = f"https://sec.report/Document/Header/?formType=DEFM14A&page={page}"
